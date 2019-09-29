c4dceeaaa077f074c192854b314beda7b3ef433e
Akira <a0z.universe@gmail.com>
2019-09-29 11:39:19 -0400

    Fixed the armState
    just did some minor changes to make the code work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FULL_AUTO_BT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FULL_AUTO_BT.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
