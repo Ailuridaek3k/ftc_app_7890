0e855f06784a9cec329aa22f0099685c0763de6e
Erin Zhao <erin_zhao@horacemann.org>
2019-12-19 11:06:19 -0500

    ftested autonomous
    - fixed up errors in r&bfoundation
    - more power to lock so hopefully we can turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FULL_AUTO_BT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FULL_AUTO_BT.java
@@ -116,7 +116,7 @@ public class FULL_AUTO_BT extends OpMode
 
         //Moves the robot towards the wall near the tray until the we are 16 inches away.
         //Detects the distance from the wall using a range sensor.
-        rangeState = new distanceMoveState(motors, distanceSensor, 16);
+        rangeState = new distanceMoveState(motors, distanceSensor, 16, 0.5);
 
         //Turns the robot around 270 degrees clockwise (which is 90 degrees ccw) so that our
         //touch sensor is facing the foundation.
@@ -133,7 +133,7 @@ public class FULL_AUTO_BT extends OpMode
         //Moves our robot until we are close to the wall near the building site. Using our
         //range sensor we can detect our distance from the wall in inches and drag the tray
         //with us to score points in the building site.
-        rangeState2 = new distanceMoveState(motors, distanceSensor, 9);
+        rangeState2 = new distanceMoveState(motors, distanceSensor, 9, 0.3);
 
         //Detaches the robot from the tray so that we can leave it in the building site.
         //Moves the armMotor upwards.
